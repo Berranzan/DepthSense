a4c97307889a6396dc8ad02c79295c09ee68ae4b
Jugoslav Djurovic <FarWatchPost@gmail.com>
2023-05-24 19:21:35 -0400

    Initial Commit

---
+++ DepthSense/DepthSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthSenseApp.swift
+//  DepthSense
+//
+//  Created by JUGOSLAV DJUROVIC on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DepthSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
